52bdc0102f938b92bd9390024816da8ba18277ae
Dexter Barrows <dexter@barrows.io>
2020-08-04 15:13:00 -0400

    add dualvec/dualvec *+- support

--- inst/include/dual.h
+++ inst/include/dual.h
@@ -20,63 +20,81 @@ struct dual {
 // --- operators ----------------------------------------------------------------------------------
 
 // --- + ----------------------------------------
-inline dual operator+(const dual &x, const dual &y) {
+inline dual operator+(const dual& x, const dual& y) {
     return dual(x.r + y.r, x.e + y.e);
 }
 template <typename A>
-inline dual operator+(const dual &x, const A &a) {
+inline dual operator+(const dual& x, const A& a) {
     return dual(x.r + a, x.e);
 }
 template <typename A>
-inline dual operator+(const A &a, const dual &x) {
+inline dual operator+(const A& a, const dual& x) {
     return x + a;
+}
+inline vector<dual> operator+(const vector<dual>& xv, const vector<dual>& yv) {
+    auto v = vector<dual>(xv.size());
+    for (uint i = 0; i < v.size(); i++)
+        v[i] = xv[i] + yv[i];
+    return v;
 }
 
 // --- - ----------------------------------------
-inline dual operator-(const dual &x, const dual &y) {
+inline dual operator-(const dual& x, const dual& y) {
     return dual(x.r - y.r, x.e - y.e);
 }
 template <typename A>
-inline dual operator-(const dual &x, const A &a) {
+inline dual operator-(const dual& x, const A& a) {
     return dual(x.r - a, x.e);
 }
 template <typename A>
-inline dual operator-(const A &a, const dual &x) {
+inline dual operator-(const A& a, const dual& x) {
     return dual(a - x.r, -x.e);
+}
+inline vector<dual> operator-(const vector<dual>& xv, const vector<dual>& yv) {
+    auto v = vector<dual>(xv.size());
+    for (uint i = 0; i < v.size(); i++)
+        v[i] = xv[i] - yv[i];
+    return v;
 }
 
 // --- * ----------------------------------------
-inline dual operator*(const dual &x, const dual &y) {
+inline dual operator*(const dual& x, const dual& y) {
     return dual(x.r*y.r, x.e*y.r + x.r*y.e);
 }
 template <typename A>
-inline dual operator*(const dual &x, const A &a) {
+inline dual operator*(const dual& x, const A& a) {
     return dual(x.r*a, x.e*a);
 }
 template <typename A>
-inline dual operator*(const A &a, const dual &x) {
+inline dual operator*(const A& a, const dual& x) {
     return x*a;
 }
-inline vector<dual> operator*(const dual &x, const vector<dual>& v) {
+inline vector<dual> operator*(const vector<dual>& xv, const vector<dual>& yv) {
+    auto v = vector<dual>(xv.size());
+    for (uint i = 0; i < v.size(); i++)
+        v[i] = xv[i]*yv[i];
+    return v;
+}
+inline vector<dual> operator*(const dual& x, const vector<dual>& v) {
     auto xv = vector<dual>(v.size());
     transform(v.begin(), v.end(), xv.begin(),
               [x](const dual& ve){ return x*ve; });
     return xv;
 }
-inline vector<dual> operator*(const vector<dual>& v, const dual &x) {
+inline vector<dual> operator*(const vector<dual>& v, const dual& x) {
     return x*v;
 }
 
 // --- / ----------------------------------------
-inline dual operator/(const dual &x, const dual &y) {
+inline dual operator/(const dual& x, const dual& y) {
     return dual(x.r/y.r, (x.r*y.e + x.e*y.r)/(y.r*y.r));
 }
 template <typename A>
-inline dual operator/(const dual &x, const A &a) {
+inline dual operator/(const dual& x, const A& a) {
     return dual(x.r/a, x.e/a);
 }
 template <typename A>
-inline dual operator/(const A &a, const dual &x) {
+inline dual operator/(const A& a, const dual& x) {
     return dual(a/x.r, (a*x.e)/(x.r*x.r));
 }
 
