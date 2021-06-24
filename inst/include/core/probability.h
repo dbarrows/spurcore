a35c9581aba312a847213eb0588d373d70931a8a
Dexter Barrows <dexter@barrows.io>
2021-06-24 13:53:32 -0400

    reorg headers under core/ + rename arma_utils -> arma, random -> probability + add stub.cpp

---
+++ inst/include/core/probability.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <RcppArmadillo.h>
+
+namespace core {
+
+inline double runif() {
+    return R::runif(0, 1);
+}
+
+inline unsigned int rpois(double lambda) {
+    return R::rpois(lambda);
+}
+
+inline double rnorm(double mean, double sd) {
+    return R::rnorm(mean, sd);
+}
+
+inline double dnorm(double x, double mean, double sd, bool log = false) {
+    return R::dnorm(x, mean, sd, log);
+}
+
+inline vec dnorm(vec x, vec mean, vec sd, bool log = false) {
+    vec p = vec(x.size());
+    for (uint i = 0; i < p.size(); i++)
+        p[i] = dnorm(x[i], mean[i], sd[i], log);
+    return p;
+}
+
+}
---
+++ src/stub.cpp
@@ -0,0 +1,6 @@
+#include <arma.h>
+#include <array3.h>
+#include <dual.h>
+#include <probability.h>
+#include <utils.h>
+#include <volume.h>
